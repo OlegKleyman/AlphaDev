80bdfd9ef0752cfbcc7fb25dcf60329a4875c666
Oleg Kleyman <Omego2K@gmail.com>
2017-05-26 10:12:11 -0400

    add scenario for loading the homepage

---
+++ tests/integration/AlphaDev.Web.Tests.Integration/Features/Homepage_feature.Steps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlphaDev.Web.Tests.Integration.Features
+{
+    using LightBDD.XUnit2;
+
+    using Xunit.Abstractions;
+
+    public partial class Homepage_feature : FeatureFixture
+    {
+        public Homepage_feature(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        private void Given_i_am_a_user()
+        {
+            
+        }
+
+        private void When_i_go_to_the_homepage()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Then_it_should_load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
