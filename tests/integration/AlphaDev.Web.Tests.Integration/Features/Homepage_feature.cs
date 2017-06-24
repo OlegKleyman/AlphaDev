c8ede76f1a084cd8112cf3697efe59e120872ce4
Oleg Kleyman <Omego2K@gmail.com>
2017-06-24 07:48:37 -0400

    use simple methods as arguments instead of pointers

--- tests/integration/AlphaDev.Web.Tests.Integration/Features/Homepage_feature.cs
+++ tests/integration/AlphaDev.Web.Tests.Integration/Features/Homepage_feature.cs
@@ -1,6 +1,7 @@
 ﻿namespace AlphaDev.Web.Tests.Integration.Features
 {
     using LightBDD.Framework;
+    using LightBDD.Framework.Scenarios.Basic;
     using LightBDD.Framework.Scenarios.Extended;
     using LightBDD.XUnit2;
 
@@ -12,20 +13,20 @@
     {
         [Scenario]
         public void Load_homepage() => Runner.RunScenario(
-            _ => Given_i_am_a_user(),
-            _ => When_i_go_to_the_homepage(),
-            _ => Then_it_should_load());
+            Given_i_am_a_user,
+            When_i_go_to_the_homepage,
+            Then_it_should_load);
 
         [Scenario]
         public void Display_nav() => Runner.RunScenario(
-            _ => Given_i_am_a_user(),
-            _ => When_i_go_to_the_homepage(),
-            _ => Then_it_should_display_navigation_links());
+            Given_i_am_a_user,
+            When_i_go_to_the_homepage,
+            Then_it_should_display_navigation_links);
 
         [Scenario]
         public void Display_latest_post() => Runner.RunScenario(
-            _ => Given_i_am_a_user(),
-            _ => When_i_go_to_the_homepage(),
-            _ => Then_it_should_display_the_latest_blog_post());
+            Given_i_am_a_user,
+            When_i_go_to_the_homepage,
+            Then_it_should_display_the_latest_blog_post);
     }
 }
