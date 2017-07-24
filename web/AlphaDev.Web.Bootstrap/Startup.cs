c1d0f4575442ee50d5bf1ee14687a1a015266f16
Omego2K <omego2k@gmail.com>
2017-07-23 20:00:16 -0400

    use scoped dependencies

--- web/AlphaDev.Web.Bootstrap/Startup.cs
+++ web/AlphaDev.Web.Bootstrap/Startup.cs
@@ -28,10 +28,9 @@ namespace AlphaDev.Web.Bootstrap
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IBlogService, BlogService>();
-            services.AddSingleton<BlogContext, Core.Data.Sql.Contexts.BlogContext>(
+            services.AddScoped<IBlogService, BlogService>();
+            services.AddScoped<BlogContext, Core.Data.Sql.Contexts.BlogContext>(
                 provider => new Core.Data.Sql.Contexts.BlogContext(Configuration.GetConnectionString("default")));
-
             services.AddMvc();
         }
 
