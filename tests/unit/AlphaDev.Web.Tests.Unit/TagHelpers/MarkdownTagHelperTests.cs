99acc81d4a2f3502c395da64942a9d0d3b214141
Omego2K <omego2k@gmail.com>
2018-02-04 18:35:09 -0500

    implement markdown parser

---
+++ tests/unit/AlphaDev.Web.Tests.Unit/TagHelpers/MarkdownTagHelperTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using AlphaDev.Web.TagHelpers;
+using FluentAssertions;
+using Markdig;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Xunit;
+
+namespace AlphaDev.Web.Tests.Unit.TagHelpers
+{
+    public class MarkdownTagHelperTests
+    {
+        [Theory]
+        [InlineData("**test**", "<p><strong>test</strong></p>\n")]
+        [InlineData("`test`", "<p><code>test</code></p>\n")]
+        [InlineData("`<s>test</s>`", "<p><code>&lt;s&gt;test&lt;/s&gt;</code></p>\n")]
+        public void ProcessShouldConvertMarkdownToHtml(string markdown, string expected)
+        {
+            var sut = GetMarkdownTagHelper();
+
+            var tagHelperOutput = new TagHelperOutput(default(string), new TagHelperAttributeList(), (_, __) =>
+            {
+                var content = new DefaultTagHelperContent();
+
+                content.SetHtmlContent(markdown);
+                return Task.FromResult<TagHelperContent>(content);
+            });
+
+            sut.Process(null, tagHelperOutput);
+
+            var writer = new StringWriter();
+
+            tagHelperOutput.Content.WriteTo(writer, HtmlEncoder.Default);
+
+            writer.ToString().ShouldBeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ProcessShouldSetTagNameToEmptyString()
+        {
+            var sut = GetMarkdownTagHelper();
+
+            var tagHelperOutput = new TagHelperOutput(default(string), new TagHelperAttributeList(), (_, __) =>
+            {
+                var content = new DefaultTagHelperContent();
+
+                content.SetHtmlContent(String.Empty);
+                return Task.FromResult<TagHelperContent>(content);
+            });
+
+            tagHelperOutput.TagName = "test";
+
+            sut.Process(null, tagHelperOutput);
+
+            tagHelperOutput.TagName.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ProcessShouldThrowArgumentNullExceptionWhenOutputArgumentIsNull()
+        {
+            var sut = GetMarkdownTagHelper();
+
+            Action process = () => sut.Process(null, null);
+
+            process.ShouldThrow<ArgumentNullException>().WithMessage("Value cannot be null.\r\nParameter name: output")
+                .Which.ParamName.ShouldBeEquivalentTo("output");
+        }
+
+        [Theory]
+        [InlineData("**test**", "<p><strong>test</strong></p>\n")]
+        [InlineData("`test`", "<p><code>test</code></p>\n")]
+        [InlineData("`<s>test</s>`", "<p><code>&lt;s&gt;test&lt;/s&gt;</code></p>\n")]
+        public async Task ProcessAsyncShouldConvertMarkdownToHtml(string markdown, string expected)
+        {
+            var sut = GetMarkdownTagHelper();
+
+            var tagHelperOutput = new TagHelperOutput(default(string), new TagHelperAttributeList(), (_, __) =>
+            {
+                var content = new DefaultTagHelperContent();
+
+                content.SetHtmlContent(markdown);
+                return Task.FromResult<TagHelperContent>(content);
+            });
+
+            await sut.ProcessAsync(null, tagHelperOutput);
+
+            var writer = new StringWriter();
+
+            tagHelperOutput.Content.WriteTo(writer, HtmlEncoder.Default);
+
+            writer.ToString().ShouldBeEquivalentTo(expected);
+        }
+
+        private MarkdownTagHelper GetMarkdownTagHelper()
+        {
+            return new MarkdownTagHelper();
+        }
+    }
+}
---
+++ web/AlphaDev.Web/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Markdig;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AlphaDev.Web.TagHelpers
+{
+    public class MarkdownTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var content = (output ?? throw new ArgumentNullException(nameof(output))).GetChildContentAsync(NullHtmlEncoder.Default)
+                .GetAwaiter().GetResult();
+
+            output.Content.SetHtmlContent(Markdown.ToHtml(content.GetContent()));
+
+            output.TagName = String.Empty;
+
+            base.Process(context, output);
+        }
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            return Task.Run(() => Process(context, output));
+        }
+    }
+}
