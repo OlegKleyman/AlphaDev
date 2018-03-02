50c57369b220c07ed6b9a411358131d33596a471
Omego2K <omego2k@gmail.com>
2018-03-02 07:00:29 -0500

    Create authentication test fixture and implement login page load test

---
+++ tests/integration/AlphaDev.Web.Tests.Integration/Features/Authentication_feature.Steps.cs
@@ -0,0 +1,25 @@
+﻿using AlphaDev.Web.Tests.Integration.Fixtures;
+using FluentAssertions;
+using LightBDD.Framework;
+using LightBDD.XUnit2;
+using Xunit.Abstractions;
+
+namespace AlphaDev.Web.Tests.Integration.Features
+{
+	public partial class Authentication_feature: WebFeatureFixture
+	{
+	    public Authentication_feature(ITestOutputHelper output, DatabaseWebServerFixture databaseWebServerFixture) : base(output, databaseWebServerFixture)
+	    {
+	    }
+
+	    private void When_I_go_to_the_login_page()
+	    {
+	        SiteTester.Login.GoTo();
+	    }
+
+	    private void Then_it_should_load()
+	    {
+	        SiteTester.Login.Title.Should().BeEquivalentTo("Login - AlphaDev");
+	    }
+	}
+}
---
+++ tests/integration/AlphaDev.Web.Tests.Integration/Features/Authentication_feature.cs
@@ -0,0 +1,23 @@
+﻿using LightBDD.Framework;
+using LightBDD.Framework.Scenarios.Basic;
+using LightBDD.Framework.Scenarios.Extended;
+using LightBDD.XUnit2;
+
+namespace AlphaDev.Web.Tests.Integration.Features
+{
+	[FeatureDescription(
+@"In order to have authorization to protected parts of the web site
+As a user
+I want to authenticate myself")]
+	public partial class Authentication_feature
+	{
+	    [Scenario]
+	    public void Login_page_should_load()
+	    {
+	        Runner.RunScenario(
+	            _ => CommonSteps.Given_i_am_a_user(),
+	            _ => When_I_go_to_the_login_page(),
+	            _ => Then_it_should_load());
+	    }
+	}
+}
