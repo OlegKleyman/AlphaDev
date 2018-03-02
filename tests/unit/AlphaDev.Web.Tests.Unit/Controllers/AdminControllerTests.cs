ff0f0642bba265f35a2d220ee38635f6168d2e90
Omego2K <omego2k@gmail.com>
2018-03-01 19:38:16 -0500

    add admin endpoint

---
+++ tests/unit/AlphaDev.Web.Tests.Unit/Controllers/AdminControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AlphaDev.Core;
+using AlphaDev.Web.Controllers;
+using AlphaDev.Web.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Optional;
+using Xunit;
+
+namespace AlphaDev.Web.Tests.Unit.Controllers
+{
+    public class AdminControllerTests
+    {
+        private AdminController GetAdminController()
+        {
+            var blogService = Substitute.For<IBlogService>();
+            blogService.GetLatest().Returns(((BlogBase) new Blog(default, null, null, default)).Some());
+
+            return GetAdminController(blogService);
+        }
+
+        private AdminController GetAdminController(IBlogService blogService)
+        {
+            return new AdminController();
+        }
+
+        [Fact]
+        public void IndexShouldReturnIndexView()
+        {
+            var controller = GetAdminController();
+
+            controller.Index().Should().BeOfType<ViewResult>();
+        }
+    }
+}
---
+++ web/AlphaDev.Web/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaDev.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
