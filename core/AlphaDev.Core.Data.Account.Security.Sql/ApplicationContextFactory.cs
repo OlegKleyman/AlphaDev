6f52684039f827776001b63412d8ee616884fe52
Omego2K <omego2k@gmail.com>
2018-03-02 07:54:39 -0500

    add migrations

---
+++ core/AlphaDev.Core.Data.Account.Security.Sql/ApplicationContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using AlphaDev.Core.Data.Account.Security.Sql.Contexts;
+using AlphaDev.Core.Data.Account.Security.Sql.Entities;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace AlphaDev.Core.Data.Account.Security.Sql
+{
+    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        private readonly IConfigurationRoot _config;
+
+        public ApplicationContextFactory()
+        {
+        }
+
+        public ApplicationContextFactory(IConfigurationRoot config)
+        {
+            _config = config;
+        }
+
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            return new ApplicationContext(
+                _config?.GetConnectionString("AlphaDevSecurity") ?? @"Data Source=(LocalDB)\v11.0;");
+        }
+    }
+}
---
+++ tests/unit/AlphaDev.Core.Data.Account.Security.Sql.Tests/ApplicationContextFactoryTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace AlphaDev.Core.Data.Account.Security.Sql.Tests
+{
+    public class ApplicationContextFactoryTests
+    {
+        private ApplicationContextFactory GetApplicationContextFactory(IConfigurationRoot config)
+        {
+            return new ApplicationContextFactory(config);
+        }
+
+        [Fact]
+        public void CreateShouldReturnContextFactoryWithConnectionStringIfOnePresent()
+        {
+            var builder = new ConfigurationBuilder().AddInMemoryCollection(
+                new[] { new KeyValuePair<string, string>("connectionStrings:AlphaDevSecurity", "Data Source=(Test);") });
+
+            var factory = GetApplicationContextFactory(builder.Build());
+
+            // ReSharper disable once AssignNullToNotNullAttribute - null is allowed with this method, resharper is
+            // bonkers.
+            factory.CreateDbContext(null).Database.GetDbConnection().Should().BeOfType<SqlConnection>().Subject
+                .ConnectionString
+                .Should().BeEquivalentTo("Data Source=(Test);");
+        }
+
+        [Fact]
+        public void CreateShouldReturnContextFactoryWithDefaultConnectionStringIfConfigIsNull()
+        {
+            var factory = GetApplicationContextFactory(null);
+
+            // ReSharper disable once AssignNullToNotNullAttribute - null is allowed with this method, resharper is
+            // bonkers.
+            factory.CreateDbContext(null).Database.GetDbConnection().Should().BeOfType<SqlConnection>().Subject
+                .ConnectionString
+                .Should().BeEquivalentTo(@"Data Source=(LocalDB)\v11.0;");
+        }
+
+        [Fact]
+        public void CreateShouldReturnContextFactoryWithDefaultConnectionStringIfNoConfigIsPassed()
+        {
+            var factory = new ApplicationContextFactory();
+
+            // ReSharper disable once AssignNullToNotNullAttribute - null is allowed with this method, resharper is
+            // bonkers.
+            factory.CreateDbContext(null).Database.GetDbConnection().Should().BeOfType<SqlConnection>().Subject
+                .ConnectionString
+                .Should().BeEquivalentTo(@"Data Source=(LocalDB)\v11.0;");
+        }
+
+        [Fact]
+        public void CreateShouldReturnContextFactoryWithDefaultConnectionStringIfNonePresentInConfig()
+        {
+            var builder = new ConfigurationBuilder();
+
+            var factory = GetApplicationContextFactory(builder.Build());
+
+            // ReSharper disable once AssignNullToNotNullAttribute - null is allowed with this method, resharper is
+            // bonkers.
+            factory.CreateDbContext(null).Database.GetDbConnection().Should().BeOfType<SqlConnection>().Subject
+                .ConnectionString
+                .Should().BeEquivalentTo(@"Data Source=(LocalDB)\v11.0;");
+        }
+    }
+}
