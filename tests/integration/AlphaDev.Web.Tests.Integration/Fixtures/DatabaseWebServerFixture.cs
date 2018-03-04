5fc3b557e3d9d728b62d92540f6ac9f340fd0746
Omego2K <omego2k@gmail.com>
2018-03-04 06:37:22 -0500

    aaaaa

--- tests/integration/AlphaDev.Web.Tests.Integration/Fixtures/DatabaseWebServerFixture.cs
+++ tests/integration/AlphaDev.Web.Tests.Integration/Fixtures/DatabaseWebServerFixture.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using AlphaDev.Core.Data.Account.Security.Sql.Entities;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AlphaDev.Web.Tests.Integration.Fixtures
 {
     public class DatabaseWebServerFixture : IDisposable
     {
+        private readonly IServiceScope _serviceScope;
+
         public DatabaseWebServerFixture()
         {
+            DatabasesFixture = new DatabasesFixture();
 
+            Server = new WebServer(DatabasesFixture.ConnectionStrings);
+            var services = Server.Start();
+
+            var b = (IApplicationBuilder)services.GetService(typeof(IApplicationBuilder));
+            _serviceScope = b.ApplicationServices.CreateScope();
+            UserManager = (UserManager<User>)_serviceScope.ServiceProvider.GetService(typeof(UserManager<User>));
+
+
+
+            SiteTester = new SiteTester(new Uri(Server.Url));
         }
 
         public UserManager<User> UserManager { get; private set; }
@@ -21,6 +36,7 @@ namespace AlphaDev.Web.Tests.Integration.Fixtures
 
         public void Dispose()
         {
+            _serviceScope.Dispose();
             SiteTester.Dispose();
             Server.Dispose();
             DatabasesFixture.Dispose();
@@ -28,14 +44,7 @@ namespace AlphaDev.Web.Tests.Integration.Fixtures
 
         public void Load()
         {
-            DatabasesFixture = new DatabasesFixture();
-
-            Server = new WebServer(DatabasesFixture.ConnectionStrings);
-            var services = Server.Start();
-
-            UserManager = (UserManager<User>)services.GetService(typeof(UserManager<User>));
-
-            SiteTester = new SiteTester(new Uri(Server.Url));
+            
         }
     }
 }
