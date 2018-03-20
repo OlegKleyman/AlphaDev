b28466d40ba0cd947cf91a2c7045f3510a2dcd4d
Omego2K <omego2k@gmail.com>
2018-03-20 06:57:20 -0400

    add view and model for creating

---
+++ web/AlphaDev.Web/Models/CreatePost.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlphaDev.Web.Models
+{
+    public class CreatePost
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+    }
+}
