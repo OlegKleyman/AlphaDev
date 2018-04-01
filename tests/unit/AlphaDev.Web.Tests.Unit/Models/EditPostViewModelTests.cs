cae8b7da19e885d8ff1b2901e8a48889e01eb7cc
Omego2K <omego2k@gmail.com>
2018-04-01 15:46:08 -0400

    add edit view

---
+++ tests/unit/AlphaDev.Web.Tests.Unit/Models/EditPostViewModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AlphaDev.Web.Models;
+using FluentAssertions;
+using Optional;
+using Xunit;
+
+namespace AlphaDev.Web.Tests.Unit.Models
+{
+    public class EditPostViewModelTests
+    {
+        [Fact]
+        public void PostConstructorShouldSetProperties()
+        {
+            const string title = "title";
+            const string content = "content";
+
+            var model = new EditPostViewModel(title, content, default);
+
+            model.Should().BeEquivalentTo(new {Title = title, Content = content});
+        }
+
+        [Fact]
+        public void GetConstructorShouldSetProperties()
+        {
+            const string title = "title";
+            const string content = "content";
+            var dates = new DatesViewModel(new DateTime(2018, 1, 1), default);
+
+            var model = new EditPostViewModel(title, content, dates);
+
+            model.Should().BeEquivalentTo(new { Title = title, Content = content, Dates = dates });
+        }
+    }
+}
