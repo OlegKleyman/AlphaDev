c26f9de4532afcddc026e77d4ca0f5360884396a
Omego2K <omego2k@gmail.com>
2018-04-24 22:19:14 -0400

    refactor create/edit views to reuse view

---
+++ web/AlphaDev.Web/Models/BlogEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Optional;
+
+namespace AlphaDev.Web.Models
+{
+    public class BlogEditorViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        public Option<DatesViewModel> Dates { get; set; }
+    }
+}
