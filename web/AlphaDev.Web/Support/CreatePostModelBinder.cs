2a16c86cc0d3c8fd6cb25b82a4e6c37f53cde516
Omego2K <omego2k@gmail.com>
2018-04-27 19:59:40 -0400

    implement prefix aware model binders

--- web/AlphaDev.Web/Support/CreatePostModelBinder.cs
+++ web/AlphaDev.Web/Support/CreatePostModelBinder.cs
@@ -5,20 +5,16 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace AlphaDev.Web.Support
 {
-    public class CreatePostModelBinder : IModelBinder
+    public class CreatePostModelBinder : PrefixModelBinder
     {
-        public Task BindModelAsync(ModelBindingContext bindingContext)
+        protected override void BindModel(PrefixModelBindingContext context)
         {
-            if (bindingContext == null) throw new ArgumentNullException(nameof(bindingContext));
-
-            var title = bindingContext.ValueProvider?.GetValue("Title").FirstValue;
-            var content = bindingContext.ValueProvider?.GetValue("Content").FirstValue;
+            var title = context.GetValue("Title").FirstValue;
+            var content = context.GetValue("Content").FirstValue;
 
             var model = new CreatePostViewModel(title ?? string.Empty, content ?? string.Empty);
 
-            bindingContext.Result = ModelBindingResult.Success(model);
-
-            return Task.CompletedTask;
+            context.Result = ModelBindingResult.Success(model);
         }
     }
 }
---
+++ web/AlphaDev.Web/Support/PrefixModelBindingContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AlphaDev.Web.Support
+{
+    public class PrefixModelBindingContext
+    {
+        private readonly ModelBindingContext _bindingContext;
+        private readonly string _prefix;
+
+        public PrefixModelBindingContext(ModelBindingContext bindingContext, string prefix)
+        {
+            _bindingContext = bindingContext ?? throw new ArgumentNullException(nameof(bindingContext));
+            _prefix = prefix;
+        }
+
+        public ModelStateDictionary ModelState => _bindingContext.ModelState;
+
+        public ModelBindingResult Result
+        {
+            get => _bindingContext.Result;
+            set => _bindingContext.Result = value;
+        }
+
+        public ValueProviderResult GetValue(string key)
+        {
+            return (_bindingContext.ValueProvider ?? throw new InvalidOperationException("ValueProvider is null"))
+                .GetValue(_prefix + key);
+        }
+    }
+}
