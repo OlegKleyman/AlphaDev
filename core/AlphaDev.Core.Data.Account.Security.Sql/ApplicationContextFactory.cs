bd46542920847aa31cdd77701b62f9adeb76613c
Omego2K <omego2k@gmail.com>
2018-09-25 19:44:28 -0400

    add use implicitly annotations

--- core/AlphaDev.Core.Data.Account.Security.Sql/ApplicationContextFactory.cs
+++ core/AlphaDev.Core.Data.Account.Security.Sql/ApplicationContextFactory.cs
@@ -1,11 +1,13 @@
 ﻿using AlphaDev.Core.Data.Account.Security.Sql.Contexts;
 using AlphaDev.Core.Data.Sql;
 using AlphaDev.Core.Data.Support;
+using JetBrains.Annotations;
 
 namespace AlphaDev.Core.Data.Account.Security.Sql
 {
     public class ApplicationContextFactory : AlphaContextFactory<ApplicationContext>
     {
+        [UsedImplicitly]
         public ApplicationContextFactory() : this(null)
         {
         }
