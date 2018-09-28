986bbf3663f9ef7898e7cf1ae123acdbbf709329
Omego2K <omego2k@gmail.com>
2018-09-27 22:11:10 -0400

    add BlogContextFactoryTests

---
+++ tests/unit/AlphaDev.Core.Data.Sql.Tests.Unit/BlogContextFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AlphaDev.Test.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace AlphaDev.Core.Data.Sql.Tests.Unit
+{
+    public class BlogContextFactoryTests
+    {
+        [Fact]
+        public void DefaultConstructorShouldNotThrow()
+        {
+            Action constructor = () => new BlogContextFactory().EmptyCall();
+            constructor.Should().NotThrow();
+        }
+
+        [Fact]
+        public void ConstructorUsingConfigurerArgumentShouldNotThrow()
+        {
+            Action constructor = () => new BlogContextFactory(default).EmptyCall();
+            constructor.Should().NotThrow();
+        }
+    }
+}
