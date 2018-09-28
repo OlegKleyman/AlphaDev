7a6d228df49f1ab7e4841cfa774412bec6e6059d
Omego2K <omego2k@gmail.com>
2018-09-28 06:27:55 -0400

    add ApplicationContextFactoryTests

---
+++ tests/unit/AlphaDev.Core.Data.Account.Security.Sql.Tests.Unit/ApplicationContextFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AlphaDev.Test.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace AlphaDev.Core.Data.Account.Security.Sql.Tests.Unit
+{
+    public class ApplicationContextFactoryTests
+    {
+        [Fact]
+        public void DefaultConstructorShouldNotThrow()
+        {
+            Action constructor = () => new ApplicationContextFactory().EmptyCall();
+            constructor.Should().NotThrow();
+        }
+
+        [Fact]
+        public void ConstructorUsingConfigurerArgumentShouldNotThrow()
+        {
+            Action constructor = () => new ApplicationContextFactory(default).EmptyCall();
+            constructor.Should().NotThrow();
+        }
+    }
+}
