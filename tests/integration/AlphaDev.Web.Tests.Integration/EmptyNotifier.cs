e21e630a8c520bb61cf43edd0c4b164e3740534c
Omego2K <omego2k@gmail.com>
2019-03-30 12:05:34 -0400

    make screenshots based on environment

---
+++ tests/integration/AlphaDev.Web.Tests.Integration/EmptyNotifier.cs
@@ -0,0 +1,29 @@
+﻿using LightBDD.Core.Metadata;
+using LightBDD.Core.Notification;
+using LightBDD.Core.Results;
+
+namespace AlphaDev.Web.Tests.Integration
+{
+    public class EmptyNotifier : IScenarioProgressNotifier
+    {
+        public void NotifyScenarioStart(IScenarioInfo scenario)
+        {
+        }
+
+        public void NotifyScenarioFinished(IScenarioResult scenario)
+        {
+        }
+
+        public void NotifyStepStart(IStepInfo step)
+        {
+        }
+
+        public void NotifyStepFinished(IStepResult step)
+        {
+        }
+
+        public void NotifyStepComment(IStepInfo step, string comment)
+        {
+        }
+    }
+}
