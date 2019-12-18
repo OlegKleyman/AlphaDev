549b3cc7f94f6f2504abab2e72e254d6e0fb71b7
Omego2K <omego2k@gmail.com>
2019-12-18 14:21:35 -0500

    use pose for unit testing DateProvider

---
+++ tests/unit/AlphaDev.Core.Tests.Unit/ShimTests/DateProviderTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Pose;
+using Xunit;
+
+namespace AlphaDev.Core.Tests.Unit.ShimTests
+{
+    public class DateProviderTests
+    {
+        [NotNull]
+        private DateProvider GetDateProvider() => new DateProvider();
+
+        [Fact]
+        public void UtcNowShouldReturnCurrentTimeInUtc()
+        {
+            var shim = Shim.Replace(() => DateTime.UtcNow).With(() => new DateTime(2000, 1, 1));
+            DateTime? date = null;
+            PoseContext.Isolate(() => date = GetDateProvider().UtcNow, shim);
+            date.Should().Be(new DateTime(2000, 1, 1));
+        }
+    }
+}
