0e66bf7ca0d2a73035a2705a2bf8ac52f689dd75
Omego2K <omego2k@gmail.com>
2019-12-19 14:47:09 -0500

    refactor Exception or failure to not use reflection

--- core/AlphaDev.Optional.Extensions/Unsafe/OptionExtensions.cs
+++ core/AlphaDev.Optional.Extensions/Unsafe/OptionExtensions.cs
@@ -8,14 +8,8 @@ namespace AlphaDev.Optional.Extensions.Unsafe
     {
         public static TException ExceptionOrFailure<T, TException>(this Option<T, TException> option)
         {
-            if (option.HasValue)
-            {
-                throw new InvalidOperationException("Exception value is missing.");
-            }
-
-            // Option either always has Exception property. The "?" check is only a formality
-            // null return is acceptable if TException happens to be nullable
-            return (TException)option.GetType().GetProperty("Exception", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(option)!;
+            return option.Match(_ => throw new InvalidOperationException("Exception value is missing."),
+                exception => exception);
         }
     }
 }
